52c36d67cafdf8d7024b896481095082dccb6886
Alejandro <nichox.ojeda@gmail.com>
2017-12-30 00:56:35 -0300

    se subira cuando llegue el internet...

---
+++ Tienda/Controllers/ClientesController.cs
@@ -0,0 +1,65 @@
+﻿using Tienda.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tienda.Controllers
+{
+    public class ClientesController : Controller
+    {
+        ExamenEntities cnx;
+        public ClientesController()
+        {
+            cnx = new ExamenEntities();
+        }
+        
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+
+        public ActionResult Guardar(string rut_cliente , string nombre, string apellido, string direccion, string tipo_cliente)
+        {
+            Clientes cliente = new Clientes()
+            {
+               Rut_cliente = rut_cliente,
+               Nombre = nombre,
+               Apellido = apellido,
+               Direccion = direccion,
+               Tipo_cliente = tipo_cliente
+            };
+            cnx.Clientes.Add(cliente);
+            cnx.SaveChanges();
+
+            return View("Listado", cnx.Clientes.ToList());
+        }
+
+        public ActionResult Listado()
+        {
+
+            return View(cnx.Clientes.ToList());
+        }
+
+        public ActionResult Ficha(string rut_cliente)
+        {
+
+            return View(cnx.Clientes.Where(x => x.Rut_cliente.Equals(rut_cliente)).First());
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(string rut_cliente)
+        {
+            return View("Listado", cnx.Clientes.ToList());
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(string rut_cliente)
+        {
+            return View("Listado", cnx.Clientes.ToList());
+        }
+
+        
+    }
+}
---
+++ Tienda/Controllers/ProductosController.cs
@@ -0,0 +1,68 @@
+﻿using Tienda.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tienda.Controllers
+{
+    public class ProductosController : Controller
+    {
+        ExamenEntities cnx;
+
+        public ProductosController()
+        {
+            cnx = new ExamenEntities();
+        }
+
+        public ActionResult Listado()
+        {
+
+            return View(cnx.Productos.ToList());
+        }
+
+        public ActionResult Formulario()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Guardar(int id_producto, string nombre_producto, string codigo_barra, string familia, int precio, string descuento_tope, string descripcion)
+        {
+            Productos producto = new Productos()
+            {
+                Id_producto = id_producto,
+                Nombre_producto = nombre_producto,
+                Codigo_barra = codigo_barra,
+                Familia = familia,
+                Precio = precio,
+                Descuento_tope = descuento_tope,
+                Descripcion = descripcion
+            };
+            cnx.Productos.Add(producto);
+            cnx.SaveChanges();
+
+            return View("Listado", cnx.Productos.ToList());
+        }
+
+        public ActionResult Ficha(int id_producto)
+        {
+
+            return View(cnx.Productos.Where(x => x.Id_producto == id_producto).First());
+        }
+
+        // GET: Productos/Edit/5
+        public ActionResult Edit(int id_producto)
+        {
+            return View("Listado", cnx.Productos.ToList());
+        }
+
+        // GET: Productos/Delete/5
+        public ActionResult Delete(int id_producto)
+        {
+            return View("Listado", cnx.Productos.ToList());
+        }
+
+    }
+}
